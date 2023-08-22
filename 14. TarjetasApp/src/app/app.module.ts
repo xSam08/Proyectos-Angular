c66330c9b2edc74ffb4210947dc939a67a02c369
practicante.desarrollo <practicadesarrollo@autonoma.edu.co>
2023-08-22 08:28:52 -0500

    Components, Services and Model generation

--- 14. TarjetasApp/src/app/app.module.ts
+++ 14. TarjetasApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
+import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearTarjetaComponent,
+    ListarTarjetaComponent
   ],
   imports: [
     BrowserModule,
---
+++ 14. TarjetasApp/src/app/models/TarjetaCredito.ts
@@ -0,0 +1,18 @@
+export class TarjetaCredito {
+    id?: string;
+    titular: string;
+    numeroTarjeta: string;
+    fechaExpiracion: string;
+    cvc: number;
+    fechaCreacion: Date;
+    fechaActualizacion: Date;
+
+    constructor(titular: string, numeroTarjeta: string, fechaExpiracion: string, cvc: number) {
+        this.titular = titular;
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaExpiracion = fechaExpiracion;
+        this.cvc = cvc;
+        this.fechaCreacion = new Date();
+        this.fechaActualizacion = new Date();
+    }
+}
