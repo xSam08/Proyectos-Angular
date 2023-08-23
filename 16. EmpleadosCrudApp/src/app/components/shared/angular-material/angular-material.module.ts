55a3b87912601dc8b957473f86211caeef6158b3
practicante.desarrollo <practicadesarrollo@autonoma.edu.co>
2023-08-23 09:35:25 -0500

    Using a new Table that includes Paginator and Filter.

--- 16. EmpleadosCrudApp/src/app/components/shared/angular-material/angular-material.module.ts
+++ 16. EmpleadosCrudApp/src/app/components/shared/angular-material/angular-material.module.ts
@@ -7,6 +7,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 @NgModule({
@@ -17,14 +18,16 @@ import { MatInputModule } from '@angular/material/input';
     MatToolbarModule,
     MatTableModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatPaginatorModule
   ],
   exports: [
     MatSlideToggleModule,
     MatToolbarModule,
     MatTableModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatPaginatorModule
   ]
 })
 export class AngularMaterialModule { }
