b02c2292d39c40e1a0d19dd8853cdf9102a39607
practicante.desarrollo <practicadesarrollo@autonoma.edu.co>
2023-08-24 10:04:18 -0500

    Dashboard component and Renamed the whole Aplication.

--- 17. CulturaGeneralApp/src/app/app.component.spec.ts
+++ 17. CulturaGeneralApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PreguntasRespuestasApp'`, () => {
+  it(`should have as title 'CulturaGeneralApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PreguntasRespuestasApp');
+    expect(app.title).toEqual('CulturaGeneralApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PreguntasRespuestasApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CulturaGeneralApp app is running!');
   });
 });
